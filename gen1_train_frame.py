623ae2cee6a48a8e5405d2ccf93f12ce64a39d31
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2024-12-26 17:49:10 +0900

    add scripts

---
+++ gen1_train_frame.py
@@ -0,0 +1,23 @@
+import os
+
+# 固定パラメータ
+gpu_ids = 0
+batch_size_per_gpu = 5
+train_workers_per_gpu = 6
+eval_workers_per_gpu = 2
+mdl_cfg = "tiny"  # MDL_CFGの値を指定
+data_dir = "/home/ubuntu/gen1/pre_gen1/"  # DATA_DIRの値を指定
+
+event_frame_dts = [5, 10, 20, 50, 100]  # 必要に応じて値を追加
+
+# ループ処理
+for dt in event_frame_dts:
+    command = f"""
+    python3 RVT/train.py model=rnndet dataset=gen1 dataset.path={data_dir} wandb.project_name=RVT_{dt} \
+    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus={gpu_ids} \
+    batch_size.train={batch_size_per_gpu} batch_size.eval={batch_size_per_gpu} \
+    hardware.num_workers.train={train_workers_per_gpu} hardware.num_workers.eval={eval_workers_per_gpu} \
+    dataset.ev_repr_name="'event_frame_dt={dt}'"
+    """
+    print(f"Running command for gen1 event_frame_dt={dt}")
+    os.system(command)  # 実際にコマンドを実行
