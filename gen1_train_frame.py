0b20a211bc6238dd16aafaccf7d3a7c4f182167c
Tanaka Arata <ky.37f.9850@s.thers.ac.jp>
2024-12-31 14:40:50 +0900

    update

--- gen1_train_frame.py
+++ gen1_train_frame.py
@@ -1,23 +1,27 @@
 import os
 
 # 固定パラメータ
-gpu_ids = 0
+gpu_ids = [0]
 batch_size_per_gpu = 5
 train_workers_per_gpu = 6
 eval_workers_per_gpu = 2
 mdl_cfg = "tiny"  # MDL_CFGの値を指定
-data_dir = "/home/ubuntu/gen1/pre_gen1/"  # DATA_DIRの値を指定
+data_dir = "/home/arata22/dataset/pre_gen1"  # DATA_DIRの値を指定
 
-event_frame_dts = [5, 10, 20, 50, 100]  # 必要に応じて値を追加
+input_channels = 3  # 入力チャンネル数
+event_frame_dts = [50]  # 必要に応じて値を追加
+
+gpu_ids_str = ",".join(map(str, gpu_ids))
+
 
 # ループ処理
 for dt in event_frame_dts:
     command = f"""
-    python3 RVT/train.py model=rnndet dataset=gen1 dataset.path={data_dir} wandb.project_name=RVT_{dt} \
-    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus={gpu_ids} \
+    python3 train.py model=rnndet dataset=gen1 dataset.path={data_dir} wandb.project_name=SAST_gen1_frame_{dt} \
+    wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus="[ {gpu_ids_str} ]" \
     batch_size.train={batch_size_per_gpu} batch_size.eval={batch_size_per_gpu} \
     hardware.num_workers.train={train_workers_per_gpu} hardware.num_workers.eval={eval_workers_per_gpu} \
-    dataset.ev_repr_name="'event_frame_dt={dt}'"
+    dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels} validation.val_check_interval=10000
     """
     print(f"Running command for gen1 event_frame_dt={dt}")
     os.system(command)  # 実際にコマンドを実行
