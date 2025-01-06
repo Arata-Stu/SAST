429d42e05213019a731ce7c21438fff3e8b37d6e
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-01-06 18:15:27 +0900

    update

--- gen1_train_frame.py
+++ gen1_train_frame.py
@@ -8,6 +8,7 @@ eval_workers_per_gpu = 2
 mdl_cfg = "tiny"  # MDL_CFGの値を指定
 data_dir = "/home/arata22/dataset/pre_gen1"  # DATA_DIRの値を指定
 
+sampling = "random"
 input_channels = 3  # 入力チャンネル数
 event_frame_dts = [50]  # 必要に応じて値を追加
 
@@ -21,7 +22,8 @@ for dt in event_frame_dts:
     wandb.group_name=gen1 +experiment/gen1={mdl_cfg}.yaml hardware.gpus="[ {gpu_ids_str} ]" \
     batch_size.train={batch_size_per_gpu} batch_size.eval={batch_size_per_gpu} \
     hardware.num_workers.train={train_workers_per_gpu} hardware.num_workers.eval={eval_workers_per_gpu} \
-    dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels} validation.val_check_interval=10000
+    dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels} validation.val_check_interval=10000\
+    dataset.train.sampling={sampling} 
     """
     print(f"Running command for gen1 event_frame_dt={dt}")
     os.system(command)  # 実際にコマンドを実行
