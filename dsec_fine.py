005dc9b97d43f284becf07e7353948389adf0787
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2025-01-30 20:06:00 +0900

    dsec

---
+++ dsec_fine.py
@@ -0,0 +1,29 @@
+import os
+
+# 固定パラメータ
+gpu_ids = 0
+batch_size_per_gpu = 8
+train_workers_per_gpu = 6
+eval_workers_per_gpu = 2
+mdl_cfg = "tiny"  # MDL_CFGの値を指定
+base_data_dir = "./datasets/pre_dsec"  # DATA_DIRの値を指定
+artifact_name = ""
+
+sampling = "mixed" 
+input_channels = 3  # 入力チャンネル数
+event_frame_dts = [5, 10, 20, 100]  # 必要に応じて値を追加
+
+# ループ処理
+for dt in event_frame_dts:
+    data_dir = f"{base_data_dir}_{dt}"
+    command = f"""
+    python3 train.py model=rnndet dataset=dsec dataset.path={data_dir} wandb.project_name=SAST_dsec_frame_{dt} \
+    wandb.group_name=dsec +experiment/dsec={mdl_cfg}.yaml hardware.gpus={gpu_ids} \
+    batch_size.train={batch_size_per_gpu} batch_size.eval={batch_size_per_gpu} \
+    hardware.num_workers.train={train_workers_per_gpu} hardware.num_workers.eval={eval_workers_per_gpu} \
+    dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels} \
+    training.max_steps=200000 wandb.resume_only_weights=True wandb.artifact_name="{artifact_name}" \
+    dataset.train.sampling={sampling} 
+    """
+    print(f"Running command for dsec event_frame_dt={dt}")
+    os.system(command)  # 実際にコマンドを実行
---
+++ dsec_valid.py
@@ -0,0 +1,26 @@
+import os
+
+# 固定パラメータ
+gpu_ids = 0
+batch_size_per_gpu = 4
+mdl_cfg = "tiny"  # MDL_CFGの値を指定
+base_data_dir = "./datasets/pre_dsec"  # DATA_DIRの値を指定
+ckpt_path = ".ckpt"  # CKPT_PATHの値を指定
+
+input_channels = 3  # 入力チャンネル数
+event_frame_dts = [50]  # 必要に応じて値を追加
+
+
+# ループ処理
+for dt in event_frame_dts:
+    data_dir = f"{base_data_dir}_{dt}"
+    command = f"""
+        python3 validation.py dataset=dsec dataset.path={data_dir} checkpoint="'{ckpt_path}'" \
+        +experiment/dsec="{mdl_cfg}.yaml" hardware.gpus={gpu_ids} \
+        batch_size.eval={batch_size_per_gpu} use_test_set=1 \
+        dataset.ev_repr_name="'event_frame_dt={dt}'" model.backbone.input_channels={input_channels} model.postprocess.confidence_threshold=0.001
+        """
+
+
+    print(f"Running command for dsec event_frame_dt={dt}")
+    os.system(command)  # 実際にコマンドを実行
